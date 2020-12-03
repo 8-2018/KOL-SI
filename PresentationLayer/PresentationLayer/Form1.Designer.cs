704a764bec42567043b278619507ab9ee68d8573
8-2018 <glavonjic1504@gmail.com>
2020-12-03 11:48:19 +0100

    Kreiran izgled forme na kojoj se prikazuju podaci sa jelovnika i unosi nova stavka u jelovnik.

--- PresentationLayer/PresentationLayer/Form1.Designer.cs
+++ PresentationLayer/PresentationLayer/Form1.Designer.cs
@@ -28,13 +28,114 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listBoxMenuItems = new System.Windows.Forms.ListBox();
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.labelDescription = new System.Windows.Forms.Label();
+            this.labelPrice = new System.Windows.Forms.Label();
+            this.textBoxTitle = new System.Windows.Forms.TextBox();
+            this.textBoxDescription = new System.Windows.Forms.TextBox();
+            this.textBoxPrice = new System.Windows.Forms.TextBox();
+            this.buttonInsertMenuItem = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBoxMenuItems
+            // 
+            this.listBoxMenuItems.FormattingEnabled = true;
+            this.listBoxMenuItems.ItemHeight = 16;
+            this.listBoxMenuItems.Location = new System.Drawing.Point(16, 18);
+            this.listBoxMenuItems.Name = "listBoxMenuItems";
+            this.listBoxMenuItems.Size = new System.Drawing.Size(762, 228);
+            this.listBoxMenuItems.TabIndex = 0;
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.AutoSize = true;
+            this.labelTitle.Location = new System.Drawing.Point(13, 264);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(100, 17);
+            this.labelTitle.TabIndex = 1;
+            this.labelTitle.Text = "Naslov stavke:";
+            // 
+            // labelDescription
+            // 
+            this.labelDescription.AutoSize = true;
+            this.labelDescription.Location = new System.Drawing.Point(13, 292);
+            this.labelDescription.Name = "labelDescription";
+            this.labelDescription.Size = new System.Drawing.Size(86, 17);
+            this.labelDescription.TabIndex = 2;
+            this.labelDescription.Text = "Opis stavke:";
+            // 
+            // labelPrice
+            // 
+            this.labelPrice.AutoSize = true;
+            this.labelPrice.Location = new System.Drawing.Point(13, 352);
+            this.labelPrice.Name = "labelPrice";
+            this.labelPrice.Size = new System.Drawing.Size(45, 17);
+            this.labelPrice.TabIndex = 3;
+            this.labelPrice.Text = "Cena:";
+            // 
+            // textBoxTitle
+            // 
+            this.textBoxTitle.Location = new System.Drawing.Point(127, 261);
+            this.textBoxTitle.Name = "textBoxTitle";
+            this.textBoxTitle.Size = new System.Drawing.Size(272, 22);
+            this.textBoxTitle.TabIndex = 4;
+            // 
+            // textBoxDescription
+            // 
+            this.textBoxDescription.Location = new System.Drawing.Point(16, 312);
+            this.textBoxDescription.Name = "textBoxDescription";
+            this.textBoxDescription.Size = new System.Drawing.Size(383, 22);
+            this.textBoxDescription.TabIndex = 5;
+            // 
+            // textBoxPrice
+            // 
+            this.textBoxPrice.Location = new System.Drawing.Point(72, 349);
+            this.textBoxPrice.Name = "textBoxPrice";
+            this.textBoxPrice.Size = new System.Drawing.Size(327, 22);
+            this.textBoxPrice.TabIndex = 6;
+            // 
+            // buttonInsertMenuItem
+            // 
+            this.buttonInsertMenuItem.Location = new System.Drawing.Point(16, 394);
+            this.buttonInsertMenuItem.Name = "buttonInsertMenuItem";
+            this.buttonInsertMenuItem.Size = new System.Drawing.Size(383, 32);
+            this.buttonInsertMenuItem.TabIndex = 7;
+            this.buttonInsertMenuItem.Text = "Kreiraj stavku menija";
+            this.buttonInsertMenuItem.UseVisualStyleBackColor = true;
+            this.buttonInsertMenuItem.Click += new System.EventHandler(this.buttonInsertMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonInsertMenuItem);
+            this.Controls.Add(this.textBoxPrice);
+            this.Controls.Add(this.textBoxDescription);
+            this.Controls.Add(this.textBoxTitle);
+            this.Controls.Add(this.labelPrice);
+            this.Controls.Add(this.labelDescription);
+            this.Controls.Add(this.labelTitle);
+            this.Controls.Add(this.listBoxMenuItems);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBoxMenuItems;
+        private System.Windows.Forms.Label labelTitle;
+        private System.Windows.Forms.Label labelDescription;
+        private System.Windows.Forms.Label labelPrice;
+        private System.Windows.Forms.TextBox textBoxTitle;
+        private System.Windows.Forms.TextBox textBoxDescription;
+        private System.Windows.Forms.TextBox textBoxPrice;
+        private System.Windows.Forms.Button buttonInsertMenuItem;
     }
 }
 
